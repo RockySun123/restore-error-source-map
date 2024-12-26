9d7f25d3abccbab5c0c17bea2b31cf1473cdaca7
Rockysun <869385454@qq.com>
2024-12-26 18:40:34 +0800

    feat: 代码调整

--- src/views/ErrorView.vue
+++ src/views/ErrorView.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="about">
-    <p @click="triggerTypeError()">触发 TypeError</p>
+    <p @click="triggerTypeError">触发 TypeError</p>
     <el-divider></el-divider>
-    <p @click="triggerReferenceError()">触发ReferenceError</p>
+    <p @click="triggerReferenceError">触发ReferenceError</p>
     <el-divider> </el-divider>
-    <p @click="triggerSyntaxError()">触发SyntaxError</p>
+    <p @click="triggerSyntaxError">触发SyntaxError</p>
   </div>
 </template>
 <script lang="ts" setup>
