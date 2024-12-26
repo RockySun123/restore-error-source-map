642ee5fb8e759cc7caf188858f8733970a271927
Rockysun <869385454@qq.com>
2024-12-26 13:47:30 +0800

    feat: 将路由改为hash

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,18 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import ErrorStackParser from 'error-stack-parser'
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 
+app.config.errorHandler = (err) => {
+  const errorStack = ErrorStackParser.parse(err as Error)
+  console.log('srack', errorStack[0])
+}
+
 app.mount('#app')
+
+
